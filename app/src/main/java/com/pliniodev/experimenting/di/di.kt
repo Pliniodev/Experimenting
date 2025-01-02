79cb6b3d46e932071ce7331fcccc55effa23dd6f
Plinio Alencar <plinioprojetos@hotmail.com>
2025-01-02 15:06:52 -0300

    setup koin

---
+++ app/src/main/java/com/pliniodev/experimenting/di/di.kt
@@ -0,0 +1,7 @@
+package com.pliniodev.experimenting.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+    
+}
---
+++ feature/src/main/java/com/pliniodev/feature/di/di.kt
@@ -0,0 +1,7 @@
+package com.pliniodev.feature.di
+
+import org.koin.dsl.module
+
+val featureModule = module {
+
+}
