823f722333452a0f1257241ebe5221a29f549876
Plinio Alencar <plinioprojetos@hotmail.com>
2025-01-02 13:24:30 -0300

    move theme files

--- app/src/main/java/com/pliniodev/experimenting/theme/Theme.kt
+++ app/src/main/java/com/pliniodev/experimenting/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.pliniodev.partiturasecompania.ui.theme
+package com.pliniodev.experimenting.theme
 
 import android.app.Activity
 import android.os.Build
--- app/src/main/java/com/pliniodev/experimenting/theme/Type.kt
+++ app/src/main/java/com/pliniodev/experimenting/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.pliniodev.partiturasecompania.ui.theme
+package com.pliniodev.experimenting.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
